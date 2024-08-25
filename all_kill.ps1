92df3d1483767bfe00c6a39be8fd2f3c3a6df524
solookimototakashi <solookimototakashi@gmail.com>
2024-08-25 14:45:55 +0900

    add exe

---
+++ all_kill.ps1
@@ -0,0 +1,9 @@
+﻿function Kill-ProcessTree([int]$procId) {
+    Get-WmiObject Win32_Process -Filter "ParentProcessID=$procId" | ForEach-Object { Kill-ProcessTree($_.ProcessID) }
+    Stop-Process -ID $procId -Force
+}
+
+$processes = Get-WmiObject Win32_Process | Where-Object { $_.CommandLine -like "*akebono_crown*" }
+foreach ($process in $processes) {
+    Kill-ProcessTree($process.ProcessID)
+}
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+call C:\Users\solookimoto\GitHub\akebono_crown\env\Scripts\activate
+cd C:\Users\solookimoto\GitHub\akebono_crown
+python main_controller.py > C:\Users\solookimoto\GitHub\akebono_crown\log.txt 2>&1
